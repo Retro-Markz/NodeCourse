616640b5491f02021ee761f1ea44ea9805feeb29
Retro-Markz <cubedomarco@gmail.com>
2024-09-25 16:21:03 -0700

    app update

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-// Importar express y path
+// Importar paquetes
 import express from "express";
+import bodyParser from "body-parser";
 
 // Crear la aplicación express
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 //crear rutas
-app.use((req, res, next) => {
-  res.send("<h1>Hello</h1>");
+
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>add product</h1>");
+  next();
+});
+
+app.post("/product", (req, res, next) => {
+  res.send("<h1>product</h1>");
+  next();
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>home</h1>");
   next();
 });
 
