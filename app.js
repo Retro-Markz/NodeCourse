d9722bfd42f1da1aa1bd7eeb642c93979862a7d3
Retro-Markz <cubedomarco@gmail.com>
2024-09-26 05:02:18 -0700

    add styles

--- app.js
+++ app.js
@@ -2,18 +2,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
+import path from "path";
+// Crear la aplicación express
+const app = express();
 
 //importar las rutas
 import adminRouter from "./routes/Admin.js";
 import shopRouter from "./routes/Shop.js";
-import path from "path";
 
 // Obtener la ruta del directorio actual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Crear la aplicación express
-const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -26,5 +27,5 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
 });
 
-// Exportar el módulo app para usarlo en server.mjs
+// Exportar el módulo app para usarlo en server.js
 export default app;
